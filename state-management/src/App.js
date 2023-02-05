178353ad6399906b1915a41d1d75e9d1464fb990
deepak-pallikala <deepak.pallikala@zemosolabs.com>
2023-02-05 13:47:15 +0530

    Adding state.js to git

---
+++ state-management/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import {State} from './Assignment/State'
+
+function App() {
+  return (
+    <div className='App'>
+      <State />
+    </div>
+  );
+}
+
+export default App;
